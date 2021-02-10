bf8150653ada83af88a2118ff86ddb69f2618be8
VishalSharma2001 <vishalsharma9785812846@gmail.com>
2021-02-10 18:49:33 +0530

    Initial commit

---
+++ my_try/js53.js
@@ -0,0 +1,22 @@
+console.log("This is JavaScript");
+let name="Vishal";
+let name1="Vinay";
+let name2="Sanket";
+let name3="Ankur";
+// console.log(name);
+greating="Hello Sir"
+
+function Hello(name,greating="Hey") {
+    console.log(greating+name);
+}
+
+Hello(name,greating)
+Hello(name1,greating)
+Hello(name2,greating)
+Hello(name3)
+
+function sum(a,b,c) {
+    d=a+b+c
+    console.log(d);
+}
+sum(2,4,1)
